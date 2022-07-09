addaf6df52d11c03a03922f1da86428393649542
WMeffect <95536418+WMeffect@users.noreply.github.com>
2022-07-10 03:47:15 +0800

    站存

--- test.py
+++ test.py
@@ -1,18 +1,4 @@
-from asyncio.windows_events import NULL
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-import pandas as pd
-import numpy as np
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, Flatten, LSTM, TimeDistributed, RepeatVector
-from keras.layers import BatchNormalization
-from keras.optimizers import Adam
-from keras.callbacks import EarlyStopping, ModelCheckpoint
-from keras.utils import to_categorical
-from keras import activations
-import matplotlib.pyplot as plt
-import tensorflow as tf
-
+print("hi")
 
 
 
